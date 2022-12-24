d8c25ee2f37a24cf5cd79590633672bfa02dd2d3
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:24 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Impostor .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+-- [[ Toggle ]] -- _G.Enabled = true -- Toggles script on and off  -- [[ Imposter ]] -- _G.AutoKill = true -- Automatically kill as imposter _G.KillType = 1 -- 1) Kill people 2 at a time 2) Kill everyone (Buggy) _G.AutoSabotage = true -- Automatically Sabotage devices _G.ChosenSabotage = 1 -- If auto sabotage is enabled this will pick a certain sabotage to trigger 1) Oxygen 2) Reactor 3) Communications 4) Lighting _G.AutoLockDoors = true -- Automatically lock doors  -- [[ Innocent ]] -- _G.AutoFixSabotage = true -- Automatically fix sabotaged devices _G.AutoTask = true -- Automatically completes tasks for you  -- [[ General ]] -- _G.HidePlayer = true -- Hides you at lobby  -- [[ Code - Obfuscated with PSU https://www.psu.dev/ & discord.gg/psu ]] -- loadstring(game:HttpGet("https://raw.githubusercontent.com/0ctax/ROBLOX-Scripts/master/Impostor!/Obfuscated_Auto_Play.lua"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Very good and simple script to use for automatic farming, works on cheats like: Trixploits, Espense, Splash.
+						
+]]
