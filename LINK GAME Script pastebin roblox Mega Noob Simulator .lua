cbcbd6be7a4e662f73ef717d7ecf1f0258c318f6
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:30 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Mega Noob Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Eprx/scripts/master/meganoobsim')))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script is suitable for fast pumping! It has such features as: auto-buy, automatic transmission, farm boss, leaders, points, TP coins.
+						
+]]
