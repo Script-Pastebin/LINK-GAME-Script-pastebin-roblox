9d87dc42c532e852b1b1a482ab9c6d5d17baa561
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:52 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Tapping Mania .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/LoadstringLri132x9y0yU", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Good auto-farm for free cheat, useful features Auto-Tapp, Auto-Rebirth, All Islands!
+						
+]]
