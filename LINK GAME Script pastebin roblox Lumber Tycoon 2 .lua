9169970b3e015c61c938f03147017d77143e68ad
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:11 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Lumber Tycoon 2 .lua
@@ -0,0 +1,13 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/hnVtRcyb"))()  
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The Gui
+First Dupe
+Other Dupes
+						
+]]
