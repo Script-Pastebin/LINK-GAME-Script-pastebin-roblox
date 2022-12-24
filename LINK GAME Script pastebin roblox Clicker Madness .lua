5ce3edfdfe362636eec155031c8295635e71e61a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:34:19 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Clicker Madness .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/WetCheezit/ClickerMadness/main/ClickerMadness.lua',true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The coolest script for this game! Many functions and the most useful ones.
+						
+]]
