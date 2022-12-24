5874a7e9fb693734f981651c8297dd842995e79e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:59 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Amongst Us Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psykek66/eee/main/AUGUI.txt", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Very cool GUI for the new simulator. It has teleports, free gamepasses & more
+						
+]]
