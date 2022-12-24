8e49452b816eaa175182f5529984e9c1cb473913
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:40 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Shinobi Life 2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Pr6Qh2U4", true))()  --Loaded By Terni
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							This script has functions like: levelfarm, warfarm, killaura, killjin, autoprestige, itemteleport an
+						
+]]
