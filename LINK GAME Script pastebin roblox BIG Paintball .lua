add4f83edcef4ce9913f61585c8305e88e80720d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:36 +0100

    commit zero

--- LINK GAME Script pastebin roblox BIG Paintball .lua
+++ LINK GAME Script pastebin roblox BIG Paintball .lua
@@ -1,12 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Singularity5490/bigpaintball/main/unlockall.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							This script will unlock all the weapons in the game even donation!
-But still it's not forever after re-entering the game, everything disappears!!!
+							Games in this hub such as: UNIVERSAL ESP ARSENAL BIG PAINTBALL NINJA LEGENDS BUBBLE GUM SIM TOWER OF HELL THE TOWER OF HELL PRISON LIFE BREAK IN (STORY) BIG BRAIN SIM.
 						
 ]]
