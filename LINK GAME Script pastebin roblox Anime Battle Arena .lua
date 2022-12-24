53d748e9a987221b0cb9cc84b117ea7ad3fd5fd2
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:03 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Anime Battle Arena .lua
@@ -0,0 +1,12 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+--READ THREAD LOL  loadstring(game:HttpGet("https://raw.githubusercontent.com/travislmao/pub-scripts/main/abagui", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							NO STUN
+HITBOX EXTENDERS (Works for moves like: Chidori, Rasengan, Hamon Bottlecap, Bearing Shot, Black Goku, Krillin, Yusuke shotgun etc.)
+						
+]]
