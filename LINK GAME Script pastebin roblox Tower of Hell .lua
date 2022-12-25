ae9aef1f312f0265bc4fa0f5e58ffbe5d081f7fd
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:58:26 +0100

    commit zero

--- LINK GAME Script pastebin roblox Tower of Hell .lua
+++ LINK GAME Script pastebin roblox Tower of Hell .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/bockatta/Atta/main/Loader'))();
+while true do wait(5) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").tower.finishes.Finish.CFrame game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").tower.finishes.Finish.CFrame game:GetService("TeleportService"):Teleport(1962086868, game:GetService("Players").LocalPlayer) end
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script gives you infinite HP and you don't die!
+							AUTO FARM - SERVER HOP WORKING EASY EXPLOITS
 						
 ]]
