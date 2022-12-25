599a07d0cb10cacebdf16fd4c2fffbb1be33e1d0
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:17 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Bleach Era .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://pastebin.com/raw/v11kHwHu'), true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Cool script for auto-farm and many other features!
+						
+]]
