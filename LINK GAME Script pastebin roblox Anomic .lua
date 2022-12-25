6e475f20687e046fb7de29e91dc1e0e7f6a22bad
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:28:54 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Anomic .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet'https://raw.githubusercontent.com/Macintosh1983/fuckyouanomicmods/main/MAINSCRIPT')()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							GUI with many features for a good game!
+						
+]]
