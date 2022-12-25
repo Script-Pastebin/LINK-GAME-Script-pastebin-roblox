54ca9cbde2c32e902abd2c7ead5258b102a34c63
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:01 +0100

    commit zero

--- LINK GAME Script pastebin roblox Murder Mystery 2 .lua
+++ LINK GAME Script pastebin roblox Murder Mystery 2 .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script gives all ESP, Murder / Sheriff EXP, Speed Hack other!
+							The script for a good game and legitmate functional and easy to use GUI!
 						
 ]]
