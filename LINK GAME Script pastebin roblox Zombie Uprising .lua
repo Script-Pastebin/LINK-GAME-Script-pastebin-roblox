42d72854a645fefe81b35e5476344463f8f8ec22
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:58:01 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Zombie Uprising .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/bockatta/Atta/main/Loader'))();
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script gives you a rank and crazyrpg!
+						
+]]
