78883a1541dc2f0a685325053b144e9064ef17be
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:58:41 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Wisteria .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/NPzdpV22'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for farming and just playing with friends!
+						
+]]
