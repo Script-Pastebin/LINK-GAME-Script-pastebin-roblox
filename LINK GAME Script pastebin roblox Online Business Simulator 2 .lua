d987945220f31370b220aae7b1832f66f10262c3
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:27:53 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Online Business Simulator 2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iiDxW/Scripts/master/OBS2"))()  game:GetService("StarterGui"):SetCore("SendNotification", { Title = "RbxScripts.com"; Text = "Loaded"; Duration = 15; })
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Mini script for auto-farm for this game you can develop quickly with this script!
+						
+]]
