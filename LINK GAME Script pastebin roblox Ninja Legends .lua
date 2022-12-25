388f1475f5cb8eb7c693db3c1e871ba59fcb2dae
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:28:06 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Ninja Legends .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/7md1TcB9",true))()  game:GetService("StarterGui"):SetCore("SendNotification", { Title = "RbxScript.com"; Text = "Loaded"; Duration = 15; })
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Convenient script for this place a lot of functions both useful and additional!
+						
+]]
