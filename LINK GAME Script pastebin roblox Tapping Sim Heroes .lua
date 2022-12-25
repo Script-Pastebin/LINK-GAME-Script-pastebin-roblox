3b13f9edd9851718e23049b51c9ef1ce018d85ea
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 23:37:15 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Tapping Sim Heroes .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/Bebo-Mods-Scripts-9/master/Tapping%20Sim%20Heroes"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with the most useful features for this game!
+						
+]]
