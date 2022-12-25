291c854746f86acd3cecb4f9e75ccadfbb75e1e7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:27 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Island Royale .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nonfunctable/IFM/main/script.lua", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Little functional script but useful for this place!
+						
+]]
