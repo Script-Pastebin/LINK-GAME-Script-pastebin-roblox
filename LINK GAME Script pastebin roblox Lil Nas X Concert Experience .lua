632afb238874ffd00d30e7082226d6cc949bde49
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:27:57 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Lil Nas X Concert Experience .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+for i,v in pairs(game.Workspace.Tokens:GetDescendants()) do     if v.Name == "Neon" and "MeshPart" and "Decal" then         v.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)     end end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Gives you all tokens
+						
+]]
