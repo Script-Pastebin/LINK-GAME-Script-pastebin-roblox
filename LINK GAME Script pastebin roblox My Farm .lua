ed1a73ece03df8d018f89199e2d76f9e05d33c0a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:28:01 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox My Farm .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+local Limit = 2000000000 local Coins = game:GetService("Players").LocalPlayer.Coins.Value local Difference = (Limit-Coins)*-1  game.ReplicatedStorage.Customization.PurchaseCustomization:FireServer("House", 4, 1, Difference, "kRX6Hy.>H!qw-`ve&5XA[e!6qr[Lpu/'m_P7Vy3s")
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Giving out infinite money for a given place!
+						
+]]
