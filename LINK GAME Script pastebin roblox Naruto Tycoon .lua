de21588f15723ed95acb6c03554b00bb4a6d2e3d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:19 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Naruto Tycoon .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://reativ.ru/Scripts/Free/NT.lua", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Fast pumping in this tycoon with our script!
+						
+]]
