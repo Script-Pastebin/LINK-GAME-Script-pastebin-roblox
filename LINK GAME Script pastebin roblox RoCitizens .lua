ac92a0577b4381cfde78e2b7a0d29a8f85c33788
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:27:55 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox RoCitizens .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/XdDMJV64'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farm on this game you can develop quickly with this script!
+						
+]]
