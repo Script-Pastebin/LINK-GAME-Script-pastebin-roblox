6e576b5aa7821388e20769f158877a6bf1c45c98
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:30:47 +0100

    commit zero

--- LINK GAME Script pastebin roblox Arsenal .lua
+++ LINK GAME Script pastebin roblox Arsenal .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script on the AIM, ESP, BIG HEAD more!
+							Cool GUI for this game running on free cheats!
 						
 ]]
