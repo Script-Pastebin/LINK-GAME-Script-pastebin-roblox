a75ff88556cb704b995c1a58c1c5ee4c7728f6e0
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:57:32 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Heroes Legacy .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HimikoToga1474/Ahegao-Hub/master/Main.Lua"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for farming in this place!
+						
+]]
