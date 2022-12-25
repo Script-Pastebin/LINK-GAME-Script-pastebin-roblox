1bb9469e243dedc74dc9b1eadea2099e3fb7e68d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:59:15 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Fart Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dk4Q/Dk4QHub/master/Loader", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with many functions for farming, and just play in this place!
+						
+]]
