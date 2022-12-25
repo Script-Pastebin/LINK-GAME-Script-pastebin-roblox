8d54a7bba42a268feb29eeaa019331f34cd70c8e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:27:40 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Shindo Life 2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+_G.speed = 750   loadstring(game:HttpGet("https://raw.githubusercontent.com/reavscripts/sl2_autofarm/main/main.lua", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Updating the war Mode Farming script works on free cheats!
+						
+]]
