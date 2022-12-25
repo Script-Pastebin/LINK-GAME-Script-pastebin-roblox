c2351a09cf849f6f3dd37726c9cf0abba26b203f
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:59:06 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Lift Legends .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Allinity/Allinity-Hub/main/AllinityHubv.1.lua"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farming and the rest!
+						
+]]
