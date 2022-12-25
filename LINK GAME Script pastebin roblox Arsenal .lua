6aee1aa57baa6ace44c9d85d96c8704b1e0d9a32
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:32:04 +0100

    commit zero

--- LINK GAME Script pastebin roblox Arsenal .lua
+++ LINK GAME Script pastebin roblox Arsenal .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nonfunctable/IFM/main/script.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Little functional script but useful for this place!
+							The GUI has the following functions: EXP, SILENT AIM, WALLBAG, INF AMMO, and others.
 						
 ]]
