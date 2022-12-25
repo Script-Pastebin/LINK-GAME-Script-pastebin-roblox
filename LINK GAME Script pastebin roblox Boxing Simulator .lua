50488c2fc16fbce62647a06bb2a60c5c4359aa48
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:57:52 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Boxing Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/mY2qKBWc'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Free script for this place, with many functions for example: Auto-buy, Auto-Farm, Open-Egg, Unlock all Islands!
+						
+]]
