e2abd262c05a0a949663d6a0d0c317aef05cde15
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:28:22 +0100

    commit zero

--- LINK GAME Script pastebin roblox RoCitizens .lua
+++ LINK GAME Script pastebin roblox RoCitizens .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://pastebin.com/raw/XdDMJV64'))()
+loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for auto-farm on this game you can develop quickly with this script!
+							РЎРєСЂРёРїС‚ РЅР° Money hack, TP Hack, Speed Hack more!
 						
 ]]
