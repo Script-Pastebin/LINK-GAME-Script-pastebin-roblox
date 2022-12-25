24b125ba92319f0aaf6e11d2c48276e1fe971d74
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 23:37:00 +0100

    commit zero

--- LINK GAME Script pastebin roblox Ro-Ghoul .lua
+++ LINK GAME Script pastebin roblox Ro-Ghoul .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
--- These settings only work if _G.ServerHop is set to true -- --Turn to true to Server Hop -- _G.ServerHop = false -- Server hops if your player gets below a certain percentage of health _G.PercentageToHop = 25 -- Will server hop if you are below this percentage in health (0-99) -- Settings -- _G.AutoEquipMask = false _G.RepFarm = true --Turn ONE of these to true if server hopping -- _G.AogiriFarm = false _G.CCGFarm = false _G.HumanFarm = false _G.FarmAll = true -- Turn ONE of these to true if server hopping -- _G.PlayAsGhoul = false _G.PlayAsCCG = false  loadstring(game:HttpGet(('https://pastebin.com/raw/x9She8BF'),true))()
+loadstring(game:HttpGet('https://pastebin.com/raw/2uF3iQAf', true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script for auto-farm in this game works on all exploits!
+							Creak for farming in this game!
 						
 ]]
