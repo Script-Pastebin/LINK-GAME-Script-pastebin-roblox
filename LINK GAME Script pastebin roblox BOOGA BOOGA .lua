81dd91aaecc4c6433744529ac6bbcd2505033bc1
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 23:38:31 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox BOOGA BOOGA .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/seUgQP2R"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farming for this game!
+						
+]]
