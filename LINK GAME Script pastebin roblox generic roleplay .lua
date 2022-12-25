a3ab8f9506eb8b639ff22139ba5df5434751743d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:58:43 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox generic roleplay .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/LoadstringFNci4Qc8irF4", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for farming and just playing with friends!
+						
+]]
