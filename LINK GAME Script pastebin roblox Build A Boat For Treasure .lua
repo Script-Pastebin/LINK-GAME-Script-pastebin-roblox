1d925d8cbe73daff5c7b4ea39c88bb6a3003d5df
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:31 +0100

    commit zero

--- LINK GAME Script pastebin roblox Build A Boat For Treasure .lua
+++ LINK GAME Script pastebin roblox Build A Boat For Treasure .lua
@@ -1,25 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-function Tween(time,pos)
-    pcall(function()
-        workspace.Gravity = 0
-        game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(time, Enum.EasingStyle.Linear), {CFrame = pos}):Play() wait(time)
-        workspace.Gravity = 196.19999694824
-    end)
-end
-function Repeat()
-    Tween(1,game:GetService("Workspace").BoatStages.NormalStages.CaveStage1.DarknessPart.CFrame)
-    Tween(18,game:GetService("Workspace").BoatStages.NormalStages.CaveStage10.DarknessPart.CFrame)
-    Tween(2,game:GetService("Workspace").BoatStages.NormalStages.TheEnd.GoldenChest.TreasureChest.Top.Union.CFrame + Vector3.new(0,10,0)) pcall(function()
-    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart,game:GetService("Workspace").GoldenChest.Collider,0) end) wait(8)
-    Repeat()
-end
-Repeat()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Toxic-Mods/ProjectInsanity/main/V1"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for legit farming without any menus!
+							Script for many of the best features for this game!
 						
 ]]
