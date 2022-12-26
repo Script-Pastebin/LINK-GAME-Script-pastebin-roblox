fe49719ec459e1262f0006af310be0dab6b24d4b
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:46 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Be a Toy! .lua
@@ -0,0 +1,20 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+while wait() do
+a = workspace.Map.Codes:GetChildren()
+for i=1,#a do v = a[i]
+if v.Back.Transparency ~= 1 then
+game.RunService.Heartbeat:Wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+game.Players.LocalPlayer.Character.Humanoid.Jump = true
+end
+end
+end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script for this game works on free cheats!
+						
+]]
