1190f83a290a3a6a1c6d50a596b6a61f3f61dcdd
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:17 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Bakon .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerDan69/scripts/main/bakon_itemesp"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script activates ESP ITEM
+						
+]]
