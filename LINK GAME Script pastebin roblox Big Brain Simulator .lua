87b832fe2df5b47f95523c46894ca63ef7a35576
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:12:25 +0100

    commit zero

--- LINK GAME Script pastebin roblox Big Brain Simulator .lua
+++ LINK GAME Script pastebin roblox Big Brain Simulator .lua
@@ -1,17 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-local coin = game:GetService("Workspace").Coins:GetDescendants()
-
-for i, C in pairs(coin) do
-    if C:IsA('Part') and C.Name == "Coin" then
-        C.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position
-    end
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script on makes tp of all cohens to you in this game!
+							The script for fast leveling in this game!
 						
 ]]
