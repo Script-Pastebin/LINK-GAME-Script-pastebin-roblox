5270fb51358bfdef3ffd25ddab90e6011d0a7853
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:56:27 +0100

    commit zero

--- LINK GAME Script pastebin roblox Shark Evolution .lua
+++ LINK GAME Script pastebin roblox Shark Evolution .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HimikoToga1474/Ahegao-Hub/master/Main.Lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script gives Auto-Change other!
+							Script with the most useful features for this game!
 						
 ]]
