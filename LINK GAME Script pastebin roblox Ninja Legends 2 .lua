3751f84e84fad4d96498db45072c4f9647e26edb
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:18:38 +0100

    commit zero

--- LINK GAME Script pastebin roblox Ninja Legends 2 .lua
+++ LINK GAME Script pastebin roblox Ninja Legends 2 .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PapierLP/RobloxScripts/main/Script%20Hub.lua", true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/GJayLG/TheNexo/master/7fff4ddb03a5f83c-obfuscated.lua')))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							A script with many cool features!
+							The script for this game works on free cheats!
 						
 ]]
