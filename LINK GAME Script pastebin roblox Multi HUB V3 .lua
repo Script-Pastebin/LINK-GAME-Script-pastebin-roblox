3684d31e679780e1bfa3a3b32647c5d3235774da
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:50:57 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Multi HUB V3 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:GetObjects("rbxassetid://1468845733")[1].Source)()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script works on all roblox games!
+						
+]]
