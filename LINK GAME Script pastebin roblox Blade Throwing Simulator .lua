5c6feaa138581aa0628794cf809edfaf5f736ef7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:58:39 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Blade Throwing Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/why-png/scriptstuffz/master/bts25", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Updated gui and fixed bans
+						
+]]
