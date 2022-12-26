e92505efa83e6cbf15904cf4d99a64120edc400f
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:13:16 +0100

    commit zero

--- LINK GAME Script pastebin roblox Tower of Misery .lua
+++ LINK GAME Script pastebin roblox Tower of Misery .lua
@@ -1,11 +1,9 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/LoadstringP0a9ozepqL5J", true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
-
-							Script with afto farm and immortality
-						
+The script gives you a 100% win in this place!
 ]]
