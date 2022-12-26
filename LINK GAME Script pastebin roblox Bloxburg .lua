dc22e78d75c5b4e48a14d0ef70a387573e90ed7a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:57:58 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Bloxburg .lua
@@ -0,0 +1,13 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+local GUIHandler = require(game:GetService("Players").LocalPlayer.PlayerGui.MainGUI.Scripts.GUIHandler);
+
+GUIHandler:AlertBox("Epic Description", "Title", 5);
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							CREATE YOUR OWN ALERT BOX
+						
+]]
