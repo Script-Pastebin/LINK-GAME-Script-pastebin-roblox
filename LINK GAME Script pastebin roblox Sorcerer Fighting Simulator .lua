d22ae0ef911fe57c08e5605d7f911720e4331c96
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:50:33 +0100

    commit zero

--- LINK GAME Script pastebin roblox Sorcerer Fighting Simulator .lua
+++ LINK GAME Script pastebin roblox Sorcerer Fighting Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-
+loadstring(game:HttpGet("https://pastebin.com/raw/jKVWMtfV", true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for farming in this game or place
+							Script with the most useful features for this game!
 						
 ]]
