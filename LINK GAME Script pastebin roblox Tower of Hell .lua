273e142763878fc2fece4eddb80f9d8fe19a323e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:12:20 +0100

    commit zero

--- LINK GAME Script pastebin roblox Tower of Hell .lua
+++ LINK GAME Script pastebin roblox Tower of Hell .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-while true do wait(5) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").tower.finishes.Finish.CFrame game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").tower.finishes.Finish.CFrame game:GetService("TeleportService"):Teleport(1962086868, game:GetService("Players").LocalPlayer) end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							AUTO FARM - SERVER HOP WORKING EASY EXPLOITS
+							Cool GUI for this game running on free cheats!
 						
 ]]
