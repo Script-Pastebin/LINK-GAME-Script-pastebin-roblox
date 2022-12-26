fb7e2d6907613162450e609cb19cdf778b01df75
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:10:38 +0100

    commit zero

--- LINK GAME Script pastebin roblox Tapping Simulator .lua
+++ LINK GAME Script pastebin roblox Tapping Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("http://www.narwhalhacks.xyz/NeonHub/Main.lua", true))()
+loadstring(game:HttpGet('http://impulse-hub.xyz/ImpulseHub',true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Cool and easy script for fast pumping in this game!
+							Cool script for this game with many features!
 						
 ]]
