0ce17a53c69b6c72d0739d1a0c08de8ecf223238
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:17:22 +0100

    commit zero

--- LINK GAME Script pastebin roblox Super Power Fighting Simulator .lua
+++ LINK GAME Script pastebin roblox Super Power Fighting Simulator .lua
@@ -1,6 +1,6 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://pastebin.com/raw/9qZrQSMq'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dk4Q/Dk4QHub/master/Loader", true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
