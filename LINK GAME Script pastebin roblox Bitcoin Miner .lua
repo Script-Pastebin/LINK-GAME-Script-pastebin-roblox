66e52eeadc79a3cc626acc015a81d9fc06d82bdd
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:51:32 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Bitcoin Miner .lua
@@ -0,0 +1,19 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+while wait() do
+   local string_1 = "GeProce PTX 1080";
+   local Target = game:GetService("Workspace").Table.Cards.CardPlace.PlaceCard;
+   Target:FireServer(string_1);
+   
+   local string_1 = "GeProce PTX 1080";
+   local Target = game:GetService("ReplicatedStorage").Events.Sell;
+   Target:FireServer(string_1);
+end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farming in this game!
+						
+]]
