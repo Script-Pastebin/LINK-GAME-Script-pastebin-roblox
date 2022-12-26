8f7f6d2dd6a474e4f9cb12ac4fd997cf23268e0a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:51:34 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Clicking Simulator 2.0 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/E6wc8Xtx'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farming in this game!
+						
+]]
