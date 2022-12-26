b2058801df939c64cf1fae33feccfd390f6f5302
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:12:16 +0100

    commit zero

--- LINK GAME Script pastebin roblox Saber Simulator .lua
+++ LINK GAME Script pastebin roblox Saber Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
- while wait() do     game.ReplicatedStorage.Events.Sell:fireServer()     game.ReplicatedStorage.Events.Clicked:fireServer() end 
+
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for simple farming!
+							Cool GUI for this game running on free cheats!
 						
 ]]
