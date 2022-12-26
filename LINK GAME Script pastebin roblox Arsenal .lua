5acb13354f8e1a4a54da284f8019e8d63f7a1e3d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:19:43 +0100

    commit zero

--- LINK GAME Script pastebin roblox Arsenal .lua
+++ LINK GAME Script pastebin roblox Arsenal .lua
@@ -1,11 +1,23 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
+local Mouse = game:GetService('Players').LocalPlayer:GetMouse()
+Mouse.Button1Down:Connect(function()
+	if game:GetService('UserInputService'):IsKeyDown(Enum.KeyCode.E) and Mouse.Target then
+		for i,targetTeam in pairs(game.Players:GetChildren()) do
+			if targetTeam.Team ~= game:GetService("Players").LocalPlayer.Team then
+				local hum = game.Players.LocalPlayer.Character.HumanoidRootPart
+				local part = targetTeam.Character.HumanoidRootPart
+				part.Anchored = true
+				part.CFrame = CFrame.new(Mouse.Hit.Position + Vector3.new(0,3,0))
+			end
+		end
+	end
+end)
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							A script with many features for trolling and much more!
+							E + Click to teleport the team
 						
 ]]
