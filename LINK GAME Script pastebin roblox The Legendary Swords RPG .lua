93a0e2129be71024c7fa0a95ea0289de3fb96dfd
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:25 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox The Legendary Swords RPG .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+game.Players.LocalPlayer.RemoteFunctions.RebirthFunction:FireServer("1000000000000000000")
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for infinite rebirths in this place or mode
+						
+]]
