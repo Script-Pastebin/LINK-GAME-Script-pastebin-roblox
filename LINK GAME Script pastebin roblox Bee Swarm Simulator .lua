bf6f29eafd9f91949758eda1c4ef09eecb6ff0fe
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:54:57 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Bee Swarm Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/w3uz/Lua/main/snowflakes.lua"),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for Christmas farming in this game!
+						
+]]
