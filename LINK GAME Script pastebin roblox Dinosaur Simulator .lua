2ac4ac088784cf79f44d26e4db02fce99168fca1
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:55 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Dinosaur Simulator .lua
@@ -0,0 +1,20 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+_G.farm = false
+
+while wait() do
+if _G.farm == true then
+local A_1 = game:GetService("Workspace").GameMap.Ferns.Fern
+local A_2 = 2
+local Event = game:GetService("Workspace").GameEvents.EatPlant
+Event:FireServer(A_1, A_2)
+   end
+end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for many useful functions for this game!
+						
+]]
