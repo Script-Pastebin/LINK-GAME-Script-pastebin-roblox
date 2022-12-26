5d4acc48384e28bddbfce4ec2aaa8464a0958f7d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:20:45 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Bloxburg v2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/XB67JnAu'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for many of the perfect features for this game!
+						
+]]
