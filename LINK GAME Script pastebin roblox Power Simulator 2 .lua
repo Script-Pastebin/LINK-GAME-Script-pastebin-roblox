afc83478afceda05dcdf0352a947dd7e48fcf461
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:20:39 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Power Simulator 2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NotARetard/script/main/Power%20Simulator%202"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for many of the good features for this game!
+						
+]]
