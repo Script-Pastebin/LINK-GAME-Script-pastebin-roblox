0d317c2acfc7fc931aea53ac0702e4111a73785e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:19 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Undertale Ultimate TimeLine .lua
@@ -0,0 +1,18 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+local player = game.Players.LocalPlayer --Credits: !Bearplayz/Disaplo#8070
+
+local mouse = player:GetMouse()
+mouse.KeyDown:Connect(function(key)
+if key =="f" then
+player.Character:MoveTo(Vector3.new(4695.15, 612.402, -2723.88))
+end
+end) --Credits: !Bearplayz/Disaplo#8070
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							To use this script to issue money, click on F
+						
+]]
