24ffc73358f63d298e0eba3c91121cf2e669d69a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:12:34 +0100

    commit zero

--- LINK GAME Script pastebin roblox Saber Simulator .lua
+++ LINK GAME Script pastebin roblox Saber Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-
+loadstring(game:HttpGet("http://www.narwhalhacks.xyz/NeonHub/Main.lua", true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Cool GUI for this game running on free cheats!
+							Cool script for fast pumping in this place a lot of different functions!
 						
 ]]
