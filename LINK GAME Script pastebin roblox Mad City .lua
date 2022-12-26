7f120c86106bcd2edf1a5f9581ddd700bee38af9
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:14:16 +0100

    commit zero

--- LINK GAME Script pastebin roblox Mad City .lua
+++ LINK GAME Script pastebin roblox Mad City .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://jack1214060.xyz/jayhub",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/samandroidpro/Chicken/main/Mad%20chickon", true))();
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							A script with the most useful features and auto-robs for this game!
+							The script farms automatically re-entering the server automatically!
 						
 ]]
