e69bfd2c9aadd2433bb05a2a9c27a7a2de05baaa
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:58:37 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Relinquit .lua
@@ -0,0 +1,17 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+local args = {
+   [1] = true
+}
+while true do
+game:GetService("Players").LocalPlayer.Character.CharacterHandler.Events.Mana:FireServer(unpack(args))
+wait()
+end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script for infinite power
+						
+]]
