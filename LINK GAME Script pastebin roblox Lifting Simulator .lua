8032f48ab6b64d834936ccda241374b38b8cda24
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:15:26 +0100

    commit zero

--- LINK GAME Script pastebin roblox Lifting Simulator .lua
+++ LINK GAME Script pastebin roblox Lifting Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://pastebin.com/raw/tDS58RkZ", true))()
+
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script with the most useful features for this game!
+							Good auto-farm for this game!
 						
 ]]
