6d7c8eb153cae1feb34826222b393625536187e5
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:13:57 +0100

    commit zero

--- LINK GAME Script pastebin roblox King Piece .lua
+++ LINK GAME Script pastebin roblox King Piece .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/seven2the10th/no/main/King%20Piece'))()
+loadstring(game:HttpGet("https://pastebin.com/raw/KyP4n2rT",true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script will help you pump in this game!
+							Script for farming and just playing with friends!
 						
 ]]
