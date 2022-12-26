dd7ba5f27cf5cc4094cd00901adb94d89c81897e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:12:23 +0100

    commit zero

--- LINK GAME Script pastebin roblox Prison Life .lua
+++ LINK GAME Script pastebin roblox Prison Life .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-  loadstring(game:HttpGet("https://pastebin.com/raw/uAnRAcFv", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Great script for this place, there are many useful functions and much more!
+							Simple but cool skriptets for this place for quick passage!
 						
 ]]
