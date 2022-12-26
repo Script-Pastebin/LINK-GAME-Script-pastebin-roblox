705b825485eefe0acbbb77d9aa12a0b8ebf32cf7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:14:10 +0100

    commit zero

--- LINK GAME Script pastebin roblox Anime Fighting Simulator .lua
+++ LINK GAME Script pastebin roblox Anime Fighting Simulator .lua
@@ -1,11 +1,9 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WalterCums/hub/main/Detection.lua", true))()
+_G.FasterLoading=false loadstring(game:HttpGet("https://raw.githubusercontent.com/The3Bakers/Spicy-Bagel/main/SpicyBagelHub.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
-
-							There are functions such as: Auto-Farm, Auto-Update, TP.
-						
+Script with more than 10 functions for this place!
 ]]
