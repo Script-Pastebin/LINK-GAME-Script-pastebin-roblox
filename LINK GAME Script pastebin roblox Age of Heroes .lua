4f03aca54120fcc77d5faa2704c1af175a7b41ac
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:54:53 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Age of Heroes .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/icuck/AgeofHeroesAutofarm/master/v4autofarm.lua"), true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with the most useful features for this game!
+						
+]]
