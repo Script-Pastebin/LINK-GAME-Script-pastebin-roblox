396a71028749a4a721d88d83bb682b33e94c420f
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:16:42 +0100

    commit zero

--- LINK GAME Script pastebin roblox Bubble Gum Simulator .lua
+++ LINK GAME Script pastebin roblox Bubble Gum Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://ghostbin.co/paste/ctpa/raw'))()
+loadstring(game:HttpGet("https://pastebin.com/raw/0rLVTifE"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script for fast leveling
+							Script for quick passage!
 						
 ]]
