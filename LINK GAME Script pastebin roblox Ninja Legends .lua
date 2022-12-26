ba9d7951c0543bbb996cee22ffa65698b9ea81cb
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:11:00 +0100

    commit zero

--- LINK GAME Script pastebin roblox Ninja Legends .lua
+++ LINK GAME Script pastebin roblox Ninja Legends .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://pastebin.com/raw/7md1TcB9",true))()  game:GetService("StarterGui"):SetCore("SendNotification", { Title = "RbxScript.com"; Text = "Loaded"; Duration = 15; })
+local Elements = {           "Inferno",           "Lightning",           "Frost",           "Shadow Charge",           "Masterful Wrath",           "Electral Chaos",           "Shadowfire",         }         for i,v in pairs(Elements) do            game.ReplicatedStorage.rEvents.elementMasteryEvent:FireServer(v)         end                  game:GetService("StarterGui"):SetCore("SendNotification", { Title = "RbxScript.com"; Text = "Loaded"; Duration = 15; })
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Convenient script for this place a lot of functions both useful and additional!
+							Gives you all the elements in the game!
 						
 ]]
