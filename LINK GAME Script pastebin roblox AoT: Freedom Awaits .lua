23aa60659a554fcad37e7b66d245b0343abb38f0
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:54 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox AoT: Freedom Awaits .lua
@@ -0,0 +1,12 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Daniel-Herron/Wulphram/main/AOT_FA.lua",true))()
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for many useful functions for this game!
+						
+]]
