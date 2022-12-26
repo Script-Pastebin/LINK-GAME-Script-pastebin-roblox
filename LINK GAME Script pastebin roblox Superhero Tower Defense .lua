399f767f4f242232f31831cc49e9b328c65cfd67
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:01 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Superhero Tower Defense .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/EfTf5ad1"))();
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							A script with useful but small functions!
+						
+]]
