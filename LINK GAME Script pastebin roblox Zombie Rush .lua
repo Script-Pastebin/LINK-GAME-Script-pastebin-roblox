0ae19aad3d990d02883f4127a4c0aa86ea055d0b
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:50:42 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Zombie Rush .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://pastebin.com/raw/LEqzpDW0'),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with the most useful features for this game!
+						
+]]
