4eebe7e6ab31e8ccb6a3d8ceb205dd1c9daeccbd
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:44 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Project Baki 2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BAG0N/OPM_DF/main/Baki"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script for this game works on free cheats!
+						
+]]
