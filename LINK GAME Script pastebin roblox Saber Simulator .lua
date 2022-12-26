a824d615c9ca6086504915e71562702f93f32734
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:13:03 +0100

    commit zero

--- LINK GAME Script pastebin roblox Saber Simulator .lua
+++ LINK GAME Script pastebin roblox Saber Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("http://www.narwhalhacks.xyz/NeonHub/Main.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Helios-Hub/main/Loader.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Cool script for fast pumping in this place a lot of different functions!
+							A good GUI with such functions as: Auto-Swing, Auto-Sell, Auto-Buy and others.
 						
 ]]
