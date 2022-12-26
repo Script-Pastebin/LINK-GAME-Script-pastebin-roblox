63ab9efb71105764a50800bb9da083c797167fdb
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:33 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Fishing Simulator .lua
@@ -0,0 +1,12 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+game:GetService("ReplicatedStorage").CloudClientResources.Communication.Functions.FishBiting:InvokeServer()
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script on the limited fish
+						
+]]
