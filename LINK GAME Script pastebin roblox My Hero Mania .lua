ef552c10641d9201df50f5d7c40e8347ceed8475
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:56:04 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox My Hero Mania .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Solx69/Shit-Boy-Hub-Main/main/Master.lua'))();
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with the most useful features for this game!
+						
+]]
