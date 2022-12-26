dbdc3eb37fa150ea44e0ab39b257f5793024cc7e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:52:20 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Flex Fight .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+for i = 1,1000 do
+    game.ReplicatedStorage.ChangeTime:FireServer("Anti333Exploitz123FF45324", 433, 429);
+end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+The script gives you 1000 seconds in this game!
+]]
