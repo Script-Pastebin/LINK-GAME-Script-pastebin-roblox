12e750f5742cb5e59992a8e19bf01e0853bd2eca
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:20:06 +0100

    commit zero

--- LINK GAME Script pastebin roblox Build A Boat For Treasure .lua
+++ LINK GAME Script pastebin roblox Build A Boat For Treasure .lua
@@ -1,11 +1,12 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/samandroidpro/Chicken/main/Build%20a%20Boat"))()
+loadstring(game:HttpGet("http://dirtgui.xyz/BuildABoat.lua",true))()
+
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							AUTO-FARM FREE HACK
+							Script for many of the best features for this game!
 						
 ]]
