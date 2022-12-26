74e1686c6ee25f5e825b46ed3631ee90a461ab56
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:23 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Fall of Hell .lua
@@ -0,0 +1,15 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+for i,v in pairs(workspace.Map.Content:GetDescendants()) do
+    if v:IsA("Model") and v.Name == "Damage" then
+        v:Destroy()
+    end
+end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for SKILL POINT
+						
+]]
