76cdfa36f4e77e2ba770f6e12250ab15d24fad78
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:59:07 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Kohls Admin House NBC .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Kohls%20Admin%20House/DarkKohls/GUI.lua"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script to open all commands
+						
+]]
