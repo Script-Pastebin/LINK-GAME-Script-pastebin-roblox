50819b248e0573e4218515c257f7b372cd1a849b
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:55:52 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Mining Inc Remastered .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://pastebin.com/raw/kPBj5QFJ'),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for many useful functions for this game!
+						
+]]
