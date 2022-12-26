78f5bf41252daf14dcd4a3d5833b5059123d03ed
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:56:25 +0100

    commit zero

--- LINK GAME Script pastebin roblox Sorcerer Fighting Simulator .lua
+++ LINK GAME Script pastebin roblox Sorcerer Fighting Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://pastebin.com/raw/jKVWMtfV", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/Bebo-Mods-Scripts-9/master/Sorcerer%20Fighting%20Simulator2%20OP"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script with the most useful features for this game!
+							Script for many of the best features for this game!
 						
 ]]
