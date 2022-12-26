588ffd7a9b3c12ea369e14c0e29b0bd632b773e6
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:11:42 +0100

    commit zero

--- LINK GAME Script pastebin roblox Blood Samurai 2 .lua
+++ LINK GAME Script pastebin roblox Blood Samurai 2 .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://pastebin.com/raw/BZvM7UbQ'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/canerom/cool-things/main/Blood%20Samurai%202.lua', true))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script with a lot of functions for good pumping or just playing!
+							AUTO FARM FOR EVERY QUEST DELIVER CRATE COLLECT RICE BODYGUARD  INFINITE STAMINA SEMI GOD MODE  AND MORE!
 						
 ]]
