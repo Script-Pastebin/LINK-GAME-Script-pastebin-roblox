562d27bbda74bf770823e74aa0e972a90371b7a7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:14:23 +0100

    commit zero

--- LINK GAME Script pastebin roblox Texting Simulator .lua
+++ LINK GAME Script pastebin roblox Texting Simulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Allinity/Allinity-Hub/main/AllinityHubv.1.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Very cool script for this game gives a lot of different functions!
+							The script for the total flow in this place!
 						
 ]]
