68bc7c90bf08a525c2d16edd20c3898552ccb809
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:26 +0100

    commit zero

--- LINK GAME Script pastebin roblox Jailbreak .lua
+++ LINK GAME Script pastebin roblox Jailbreak .lua
@@ -1,9 +1,9 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/piglex9/jailbreak-ice-tray/main/Main.lua',true))()
+loadstring(game:HttpGet('http://alpha1004.tech/scripts/AlphaX.lua'))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
-Script with more than 15 functions for this place!
+Script with many functions more than 15 functions for this game
 ]]
