39fbb4db4e899a96eda18746635df66931cadbca
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:58 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Build Battle! .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+local A_1 = 5000 local Event = game:GetService("ReplicatedStorage").Events.SubmitVote Event:FireServer(A_1)
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+The script gives you a 100% chance to win!
+]]
