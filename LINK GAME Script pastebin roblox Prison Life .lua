34e57a3b73eecf8eff29c913e864488fc112272d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:43 +0100

    commit zero

--- LINK GAME Script pastebin roblox Prison Life .lua
+++ LINK GAME Script pastebin roblox Prison Life .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Allinity/Allinity-Hub/main/AllinityHubv.1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SkidsLubScr1pts/ScxredLix/main/ScxredLixHub"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for trolling players and friends)
+							GUI for trolling friends or ordinary players)
 						
 ]]
