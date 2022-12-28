bc5e0eebb90e250de05fef4cc24fc556e3740622
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:52 +0100

    commit zero

--- LINK GAME Script pastebin roblox Jailbreak .lua
+++ LINK GAME Script pastebin roblox Jailbreak .lua
@@ -1,11 +1,9 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://pastebin.com/raw/Vs23XVsY"))()
+loadstring(game:HttpGet('http://alpha1004.tech/scripts/AlphaX.lua'))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
-
-							A script with many cool and useful features!
-						
+Script with more than 15 functions for this place!
 ]]
