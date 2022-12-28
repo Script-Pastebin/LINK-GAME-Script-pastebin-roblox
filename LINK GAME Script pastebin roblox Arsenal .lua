01f706000ad70d42254293637dd81af43a124a83
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:07 +0100

    commit zero

--- LINK GAME Script pastebin roblox Arsenal .lua
+++ LINK GAME Script pastebin roblox Arsenal .lua
@@ -1,19 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-getgenv().AimPart = "Head" -- For R15 Games: {UpperTorso, LowerTorso, HumanoidRootPart, Head} | For R6 Games: {Head, Torso, HumanoidRootPart}
-getgenv().AimlockToggleKey = "X" -- Toggles Aimbot On/Off
-getgenv().AimRadius = 30 -- How far away from someones character you want to lock on at
-getgenv().ThirdPerson = false -- Locking onto someone in your Third Person POV
-getgenv().FirstPerson = true -- Locking onto someone in your First Person POV
-getgenv().WallCheck = true -- Check if Target is behind a wall
-getgenv().TeamCheck = true -- Check if Target is on your Team (True means it wont lock onto your teamates, false is vice versa) (Set it to false if there are no teams)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zxciaz/Universal-Scripts/main/Aimbot", true))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Allinity/Allinity-Hub/main/AllinityHubv.1.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for an aim bot for a legit game
+							A script with many useful functions for this place!
 						
 ]]
