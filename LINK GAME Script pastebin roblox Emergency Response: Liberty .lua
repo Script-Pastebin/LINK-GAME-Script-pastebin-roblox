2d4762d1d015344ca1aa6d875476c76e55c9400f
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:28 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Emergency Response: Liberty .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://thisstuff.xyz/Api/WOAHSCRIPTS/6412498317-0340073397/5BQAyAwbLEBEagEffWMQCvreyPOqmjilAG0KjMLRxpxtV0dzrPmnDHYZbtLHLIajzwhx7KUVWbebxlWb961hdsBcwrO8hteQXouN", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farming and teleports for this game!
+						
+]]
