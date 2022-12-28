3bdb38c030e56231fd933cea27c73cfcc9fcf89c
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:41 +0100

    commit zero

--- LINK GAME Script pastebin roblox Grand Piece Online .lua
+++ LINK GAME Script pastebin roblox Grand Piece Online .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
---Grand Piece Online (Chest Autofarm) --The GUI will be destroyed on death, don't worry though it will still work as intended. --Credits: The infamous scammer Urbanstorm made this <3  --(as everyone just loves to call me.) loadstring(game:HttpGet("https://pastebin.com/raw/A4MWkZay"))()
+loadstring(game:HttpGet('https://pastebin.com/raw/495La6pm'))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							The script enables you to Crate Auto-Farm
+							Minimalistic GUI for this game with the most useful feature!
 						
 ]]
