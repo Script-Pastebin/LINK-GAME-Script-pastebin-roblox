36e6b922417b3b9f70bb67e9617b43a7bfbbbe43
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:14 +0100

    commit zero

--- LINK GAME Script pastebin roblox Speed Run SImulator .lua
+++ LINK GAME Script pastebin roblox Speed Run SImulator .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet(('https://gist.githubusercontent.com/Urbanstorms/8f8759d01ac16de28617536743361301/raw/cd951952cf962bddc3654d1f05d2ffda4a03581c/Speed%2520Run%2520Simulator'),true))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Allinity/Allinity-Hub/main/AllinityHubv.1.lua"))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script with the most useful features for this game!
+							The script for the total flow in this place!
 						
 ]]
