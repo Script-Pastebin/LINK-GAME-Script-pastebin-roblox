01b6d9b7a478fe0c83f2c1359ee595c8d17c58d6
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:03 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Welcome to Bloxburg .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('http://alpha1004.tech/scripts/AlphaX.lua'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farm or auto-construction!
+						
+]]
