8010a3114782f96009f5cf42fd38e9945f5b1a6e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:24 +0100

    commit zero

--- LINK GAME Script pastebin roblox A Universal Time .lua
+++ LINK GAME Script pastebin roblox A Universal Time .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/ttd1108/Script/master/AUniversalTime'), true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RaptorsSexy/Cracks/main/NoirLikesDick.txt"))();
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script with the most useful features for this game!
+							Script for a legit game or Vice versa
 						
 ]]
