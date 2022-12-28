53cf9eb13ef1d9bf8523c48d2b9d35023691536e
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:56 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Russia RP .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+for i,v in pairs(workspace.Other:GetChildren()) do if (v:FindFirstChild("Give") and v.Name == "CedGoTruck") then game.RunService.RenderStepped:Connect(function() fireclickdetector(v.Box.ClickDetector) game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(math.random(-1000,1000),1,1)) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(568.269, 2.51562, -483.216) end) end end
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for auto-farming in this game!
+						
+]]
