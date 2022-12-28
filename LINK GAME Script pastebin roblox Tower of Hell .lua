d5fd70a0f0b911eea3688f0761d49a7fd03ca191
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:58 +0100

    commit zero

--- LINK GAME Script pastebin roblox Tower of Hell .lua
+++ LINK GAME Script pastebin roblox Tower of Hell .lua
@@ -1,11 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet("https://gist.githubusercontent.com/BloxiYT/85f97364b6367baac677e109488c9905/raw/c2972a2b892c4193509ddb2fe1ba3061435e1a8a/gistfile1.txt"))()
+loadstring(game:HttpGet('http://alpha1004.tech/scripts/AlphaX.lua'))()
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							Script for this game with other functions
+							Script for farming in this game!
 						
 ]]
