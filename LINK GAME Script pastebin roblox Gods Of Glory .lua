c9b1913e265dbd1d95388c9ef94222e85506d4b8
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:53:35 +0100

    commit zero

---
+++ LINK GAME Script pastebin roblox Gods Of Glory .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+local table_1 = { ["Value"] = "inf" }; local Target = game:GetService("ReplicatedStorage").Remotes.GiveMana; Target:FireServer(table_1);
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script for very fast pumping in this place!
+						
+]]
